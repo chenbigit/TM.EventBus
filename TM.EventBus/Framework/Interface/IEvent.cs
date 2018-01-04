ef3ff50d98dedcc7fb5e000b4a1dee0387dee9ea
陈璧 <553101640@qq.com>
2018-01-04 17:24:25 +0800

    添加项目文件。

---
+++ TM.EventBus/Framework/Interface/IEvent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TM.EventBus.Framework.Interface
+{
+    /// <summary>
+    /// 消息通信渠道
+    /// </summary>
+    public interface IEvent
+    {
+        Guid id { get; set; }
+        DateTime Timestamp { get; set; }
+    }
+
+
+    public interface IEventHandler
+    {
+        Task<bool> HandleAsync(IEvent @event, CancellationToken cancellationToken = default);
+        bool CanHandle(IEvent @event);
+    }
+
+
+    public interface IEventHandler<in T> : IEventHandler
+    where T : IEvent
+    {
+        Task<bool> HandleAsync(T @event, CancellationToken cancellationToken = default);
+    }
+
+
+}
